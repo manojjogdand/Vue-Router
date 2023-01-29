05001248cfbd09070c2b52bf242f64725893a720
manojjogdand <62006294+manojjogdand@users.noreply.github.com>
2023-01-30 00:15:26 +0530

    router-5

---
+++ router-5/src/views/PageNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="parent">
+     <h1 style="color: red;">404 : Page Not Found...!!!</h1>
+   </div>
+ </template>
+ 
+ <script setup>
+ 
+ </script>
+ 
+ <style>
+ 
+ </style>
